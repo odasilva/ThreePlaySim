3bcc9b3836938183a477031e910bfeae82dc9922
odasilva <odasilva@iut.univ-paris8.fr>
2015-02-01 14:56:49 +0100

    Correct SimulationWar

--- WarPlaySim/SimulationWar.cs
+++ WarPlaySim/SimulationWar.cs
@@ -18,36 +18,15 @@ namespace ThreePlaySim.WarPlaySim
 	public class SimulationWar : SimulationAbstraite
 	{
         public string[,] map;
-        public List<Personnage> armeA;
-        public List<Personnage> armeB;        
+        public Armee armeA;
+        public Armee armeB;        
         public int x = 10;
         public int y = 10;
 
         public SimulationWar(String xmlContent)
             : base()
 		{
-			map = new string [x,y];
-			armeA = new List<Personnage>();
-			armeB = new List<Personnage>();
 			
-			armeA.Add(new Princesesse("PrinA"));
-			armeA.Add(new Fantassin("FantA"));
-			armeA.Add(new Cavalier("CavaA"));
-			                   
-			armeB.Add(new Princesesse("PrinB"));
-			armeB.Add(new Archer("ArchB"));
-			armeB.Add(new Cavalier("CavaB"));
-			
-			for(int i = 0; i<x; i++){
-				for(int j = 0; j<y; j++){
-					if(i == 0 && j == (y/2))
-						map[i,j] = " P ";
-					else if(i == 1)
-						map[i,j] = " ";
-					else
-						map[i,j] = " - ";
-				}
-			}
 		}
 		
 		public string AfficherTous()
